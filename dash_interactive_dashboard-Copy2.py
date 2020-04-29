e1fb7da40c9e83f00e06184b15806982b7a8a723
Tyler <tyler.drumheller@gmail.com>
2020-04-29 12:47:00 -0400

    Further Refinement of Dashboard

--- dash_interactive_dashboard-Copy2.py
+++ dash_interactive_dashboard-Copy2.py
@@ -15,6 +15,7 @@ games = df.groupby(['game_id','game_name']).count().reset_index()[['game_id','ga
 game_list = games.apply(lambda x : [x['game_id'], x['game_name']],axis=1)
 
 app.layout = html.Div([
+    html.Label(children='Select Game Here'),#html.Img(src='/NFL_Logo.png'),
     dcc.Dropdown(
         id='xaxis-column',
         options=[{'label': i[1], 'value': i[0]} for i in game_list],
@@ -24,15 +25,38 @@ app.layout = html.Div([
                     display='inline-block',
                     verticalAlign="middle",
                 )
-#         placeholder="Select Answer"
     ),
-    dcc.Graph(id='graph-div')
-#     ,dash_table.DataTable(
-#     id='table',
-#     data=[]
-#     )
+    dcc.Graph(id='graph-div'),
+    html.Label(children='Home Team Total Yards'),html.Div(id='home-yards'),
+    html.Label(children='Away Team Total Yards'),html.Div(id='away-yards')
 ]
 )
+
+# Callback for Away Yards - Sum isn't filtering on team or Yards are off
+@app.callback(
+    Output(component_id='home-yards', component_property='children'),
+    [Input(component_id='xaxis-column', component_property='value')]
+)
+def update_home_yards(input_value):
+    dff = df[df['game_id'] == input_value]
+    
+    team = dff['home_team'].unique()[0]
+    
+    return dff[dff['posteam']==team]['ydsnet'].sum()
+
+# Callback for Away Yards - Sum isn't filtering on team or Yards are off
+@app.callback(
+    Output(component_id='away-yards', component_property='children'),
+    [Input(component_id='xaxis-column', component_property='value')]
+)
+def update_away_yards(input_value):
+    dff = df[df['game_id'] == input_value]
+    
+    team = dff['away_team'].unique()[0]
+    
+    return dff[dff['posteam']==team]['ydsnet'].sum()
+
+
 
 @app.callback(
     Output(component_id='graph-div', component_property='figure'),
